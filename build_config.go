c97f8d8c5144852d1537189d652da026d23f68fd
IkyMax <Iky_Max_@outlook.com>
2024-12-14 14:45:45 -0600

    Fix for the pelican panel

--- build_config.go
+++ build_config.go
@@ -4,11 +4,11 @@ package main
 
 const VERSION = "2.1.0"
 
-const REPOSITORY_URL = "https://github.com/unmojang/drasl"
+const REPOSITORY_URL = "https://github.com/limbo-studios/drasl"
 
 const LICENSE = "GPLv3"
 const LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.en.html"
 
-const DEFAULT_DATA_DIRECTORY = "/usr/share/drasl"
-const DEFAULT_STATE_DIRECTORY = "/var/lib/drasl"
-const DEFAULT_CONFIG_DIRECTORY = "/etc/drasl"
+const DEFAULT_DATA_DIRECTORY = "/home/container/data"
+const DEFAULT_STATE_DIRECTORY = "/home/container/state"
+const DEFAULT_CONFIG_DIRECTORY = "/home/container"
