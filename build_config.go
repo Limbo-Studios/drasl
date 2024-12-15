ab7cf6146cdeb7f439470ac43759f72a0f7fe4c2
IkyMax <Iky_Max_@outlook.com>
2024-12-14 22:58:17 -0600

    Attempt to fix Drasl Data on pelican panel

--- build_config.go
+++ build_config.go
@@ -9,6 +9,6 @@ const REPOSITORY_URL = "https://github.com/limbo-studios/drasl"
 const LICENSE = "GPLv3"
 const LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.en.html"
 
-const DEFAULT_DATA_DIRECTORY = "/home/container/data"
-const DEFAULT_STATE_DIRECTORY = "/home/container/state"
+const DEFAULT_DATA_DIRECTORY = "/usr/share/drasl"
+const DEFAULT_STATE_DIRECTORY = "/home/container/data"
 const DEFAULT_CONFIG_DIRECTORY = "/home/container"
